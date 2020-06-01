fc916f9ce895cce7c2951b4d0e426af6c7c1222a
Siva Krishna <shivataraya@gmail.com>
2020-06-01 10:57:06 -0400

    Updates

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = "Failed to connect to database";
+  constructor() {
+    super("Failed to connect to database");
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
+}
